91f570a2396136f1bd2c8dc53a5b449e5077584d
Oleksii Mosunov <alexmosunov@gmail.com>
2020-01-24 20:56:45 +0200

    uploading app

---
+++ I am rich/I am rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Alex Mosunov on 1/22/20.
+//  Copyright © 2020 Alex Mosunov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
