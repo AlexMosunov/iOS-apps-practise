ddf79ad2405a567276444e662b131c5e63b65f72
Oleksii Mosunov <alexmosunov@gmail.com>
2020-02-01 17:54:28 +0200

    uploading files

---
+++ eggTimer/EggTimer/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+
+    let eggTimes = [
+        "Soft": 300,
+        "Medium": 420,
+        "Hard": 720
+    ]
+    var totalTime = 0
+    var secondsPassed = 0
+    var player: AVAudioPlayer!
+    var timer = Timer()
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle! // "Soft", "Medium", "Hard"
+        totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        
+    }
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @objc func updateCounter() {
+        if secondsPassed < totalTime {
+//            print("\(totalTime - secondsPassed) seconds")
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+        } else {
+            timer.invalidate()
+            titleLabel.text = "DONE!"
+            playSound(soundName: "alarm_sound")
+        }
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+}
---
+++ eggTimer/README.md
@@ -0,0 +1,14 @@
+
+
+# Egg Timer
+
+a beautiful egg timer app to boil your eggs to perfection depending on how you prefer your eggs. 
+
+
+* Swift Collection types - Dictionaries
+* The Swift Timer API
+* Conditional statements - IF/ELSE
+* Conditional statements - Switch
+* Functions with outputs
+* How to use the ProgressView
+
