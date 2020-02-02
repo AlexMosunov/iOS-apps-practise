2779bda7a8df9da7c0b71923ca967f98bc60c288
Oleksii Mosunov <alexmosunov@gmail.com>
2020-02-02 17:24:44 +0200

    uploading app

---
+++ Quizzler-multiple-opt/Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,86 @@
+
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var Button1: UIButton!
+    @IBOutlet weak var Button2: UIButton!
+    @IBOutlet weak var Button3: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var quizBrain = QuizBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateUI()
+    }
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle!
+        
+        
+        
+        
+        let userGotItRight = quizBrain.checkAnswer(userAnswer: userAnswer)
+        
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+        quizBrain.nextQuestion()
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    }
+    
+    @objc func updateUI() {
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        
+        let answerChoices = quizBrain.getAnswers()
+        
+        Button1.setTitle(answerChoices[0], for: .normal)
+        Button2.setTitle(answerChoices[1], for: .normal)
+        Button3.setTitle(answerChoices[2], for: .normal)
+        
+        Button1.backgroundColor = UIColor.clear
+        Button2.backgroundColor = UIColor.clear
+        Button3.backgroundColor = UIColor.clear
+        
+        
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
